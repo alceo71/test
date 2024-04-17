ee5b80645c8b46df97c778391199b4bdab9cfe0a
giovanni.zorzetti_external@wartsila.com <giovanni.zorzetti_external@wartsila.com>
2024-04-17 23:11:21 +0200

    Start

---
+++ app/src/main/java/it/interfase/prototipo/oproximitygps/OutputProximityGps.java
@@ -0,0 +1,26 @@
+package it.interfase.prototipo.oproximitygps;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import it.interfase.prototipo.R;
+
+public class OutputProximityGps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_output_proximity_gps);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
